fff712b3315003e9ea29d5ffa3573d7eb3830646
RuslanSerdiuk <ruslan.serdyuk1996@gmail.com>
2022-05-16 17:33:44 +0300

    Add Jenkins Task

---
+++ CICD/Task_1/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["ffi",["~> 1.0"]],["putty-key",["~> 1.0"]],["vagrant-multi-putty",["= 1.6.0"]]],"checksum":"c38f660e3f24945bf67bf64a34836e4dfce10c885419e1086441fbc52dbcd23e","vagrant_version":"2.2.19"}
