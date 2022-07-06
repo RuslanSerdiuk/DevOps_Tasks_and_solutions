20ed4e32da7d3c08d3e6f52e101210119c517cc7
RuslanSerdiuk <ruslan.serdyuk1996@gmail.com>
2022-07-06 12:57:24 +0300

    Upd: deleting unnecessary files from Terraform branch

--- Linux/README.md
+++
@@ -1,1 +0,0 @@
-# Tasks 1-2, these are examples of several useful commands that may come in handy at work
--- Vagrant_and_Network/Network_Task_2/merge_request_with_del_branch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl --request POST --header "PRIVATE-TOKEN: $API_KEY" "https://git.epam.com/api/v4/projects/122669/merge_requests?source_branch=issue&target_branch=master&title=testMR&remove_source_branch=true" | ./jq-win64.exe '.'
