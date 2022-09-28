490854006e0d832003374110bfeb812cdc7af953
RuslanSerdiuk <ruslan.serdyuk1996@gmail.com>
2022-09-28 11:24:10 +0300

    Clean "Terraform" module

--- Vagrant_and_Network/Network_Task_2/merge_request_without_del_branch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl --request POST --header "PRIVATE-TOKEN: $API_KEY" "https://git.epam.com/api/v4/projects/119684/merge_requests?source_branch=issue&target_branch=master&title=testMR&remove_source_branch=false" | ./jq-win64.exe '.'
--- Vagrant_and_Network/Network_Task_2/search.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl --header "PRIVATE-TOKEN: $API_KEY" "https://git.epam.com/api/v4/users?search=andrii_kurapov@epam.com" | ./jq-win64.exe '.'
