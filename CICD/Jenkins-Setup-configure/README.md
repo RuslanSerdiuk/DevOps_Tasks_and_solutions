88978cfd9af25f1fd44a2500e2439f38d15dbac1
Ruslan Serdiuk <Ruslan.serdiuk.w@gmail.com>
2022-11-25 14:15:14 +0200

    Clean branch + fix lambda.py + add README

---
+++ CICD/Jenkins-Setup-configure/README.md
@@ -0,0 +1,3 @@
+# Jenkins [Setup and Configure]
+
+#### TASK:
--- CICD/Jenkins-Setup-configure/lambda.py
+++ CICD/Jenkins-Setup-configure/lambda.py
@@ -1,4 +1,4 @@
-def lambda_handler(event, context):
+def handler(event, context):
     message = 'Hello {} {}!'.format(event['first_name'], event['last_name'])  
     return { 
         'message' : message
