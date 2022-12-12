1a87ef2df57ef7ef2de101baf91537879ed9bbfd
Ruslan Serdiuk <Ruslan.serdiuk.w@gmail.com>
2022-12-12 14:42:24 +0200

    added new lambda

--- AWS/CodePipeline-CodeDeploy/lambda.py
+++ AWS/CodePipeline-CodeDeploy/lambda.py
@@ -1,5 +1,19 @@
-def handler(event, context):
-    message = 'Hello {} {}!'.format(event['first_name'], event['last_name'])  
-    return { 
-        'message' : message
-    }
+import json
+import uuid
+
+GET_RAW_PATH = "/test"
+POST_RAW_PATH = "/testjson"
+
+def lambda_handler(event, context):
+    print(event)
+    if event['rawPath'] == GET_RAW_PATH:
+        print('Start request for test')
+        personId = event['queryStringParameters']['personId']
+        print("Received request with personId=" + personId)
+        return { "firstName": "Ruslan" + personId, "lastName": "Serdiuk", "email": "Ruslan.serdiuk.w@gmail.com" }
+    elif event['rawPath'] == POST_RAW_PATH:
+        print('Start request for testjson')
+        decodedEvent = json.loads(event['body'])
+        firstName = decodedEvent['firstName']
+        print('Received request with firstName=' + firstName)
+        return { "personId": str(uuid.uuid1())}
