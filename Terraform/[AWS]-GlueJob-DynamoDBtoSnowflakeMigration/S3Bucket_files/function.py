a3ffa9ad1c42ad8197d0fa63bf5a67744f7277a3
Ruslan Serdiuk <Ruslan.serdiuk.w@gmail.com>
2023-01-04 16:08:50 +0200

    Function.py - WORKS!

--- Terraform/[AWS]-GlueJob-DynamoDBtoSnowflakeMigration/S3Bucket_files/function.py
+++ Terraform/[AWS]-GlueJob-DynamoDBtoSnowflakeMigration/S3Bucket_files/function.py
@@ -3,12 +3,13 @@ import json
 
 http = urllib3.PoolManager()
 
+
 def lambda_handler(event, context):
-    url = "https://hooks.slack.com/services/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"
+    url = "https://hooks.slack.com/services/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"
     msg = {
         "channel": "#alarm-for-the-glue-job",
         "username": "WEBHOOK_USERNAME",
-        "text": json.loads(event["Records"][0]["Sns"]["Message"]),
+        "text": "Glue-job 'CommService_DynamoDB_to_s3' - " + event["message"]["detail"]["state"],
         "icon_emoji": "",
     }
 
@@ -16,9 +17,8 @@ def lambda_handler(event, context):
     resp = http.request("POST", url, body=encoded_msg)
     print(
         {
-            "message": json.loads(event["Records"][0]["Sns"]["Message"]),
+            "message": event["message"]["detail"]["state"],
             "status_code": resp.status,
             "response": resp.data,
         }
     )
-
